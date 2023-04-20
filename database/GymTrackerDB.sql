d379d9d7cf8624932486adb9038fe228d6f51be4
Alex <97796170+pvk04@users.noreply.github.com>
2023-04-20 09:15:26 +0300

    изменен код создания бд

--- database/GymTrackerDB.sql
+++ database/GymTrackerDB.sql
@@ -1,12 +1,16 @@
 create database GymTracker;
+use GymTracker;
 
 create table Users(
 	UserId int primary key auto_increment,
     UserNickname varchar(50) not null unique,
+    HeightInFt tinyint(1) not null default 0, -- if 0 - kg, else in ft
+    WeightInLb tinyint(1) not null default 0, -- if 0 - kg, else Lb
+    DistanceIn tinyint(1) not null default 0, -- if 0 - metres, else miles
     UserHeight int null check(UserHeight >= 0), -- measure types managment?
     UserDateBirth date not null,
     UserWeight int null check(UserWeight >= 0), -- measure types managment?
-    UserEmail varchar(80) not null unique,
+    UserEmail varchar(100) not null unique,
     UserPassword varchar(20) not null -- password hash max length?
 );
 
@@ -20,18 +24,18 @@ create table Workout(
     foreign key (UserId) references Users(UserId)
 );
 
+create table ExerciseMeasureTypes(
+	ExerciseMeasureTypeId int primary key auto_increment,
+    ExerciseMeasureTypeName varchar(50) not null unique,
+    ExerciseMeasureTypeShortName varchar(10) not null unique
+);
+
 create table Exercises(
 	ExerciseId int primary key auto_increment,
     ExerciseName varchar(100) not null unique,
     ExerciseMeasureType int not null,
     ExerciseDescription varchar(250) null, -- продумать связь созданных упражнений, с пользователем, создавшим их
     foreign key (ExerciseMeasureType) references ExerciseMeasureTypes(ExerciseMeasureTypeId)
-);
-
-create table ExerciseMeasureTypes(
-	ExerciseMeasureTypeId int primary key auto_increment,
-    ExerciseMeasureTypeName varchar(50) not null unique,
-    ExerciseMeasureTypeShortName varchar(10) not null unique
 );
 
 create table SetTypes(
@@ -51,17 +55,28 @@ create table WorkoutSet(
     WorkoutSetDistance int null,
     WorkoutSetNote varchar(1000) null,
     foreign key (WorkoutId) references Workout(WorkoutId),
-    foreign key (ExerciseId) references Exercises(ExerciseId),
-    foreign key (WorkoutSetWeightMeasureType) references WeightMeasureTypes(WorkoutSetWeightMeasureTypeId)
+    foreign key (ExerciseId) references Exercises(ExerciseId)
 ); 
+
+create table SuplementManufacturers(
+	SuplementManufacturerId int primary key auto_increment,
+    SuplementManufacturerName varchar(150) not null,
+    SuplementManufacturerCountry varchar(150) not null
+);
+
+create table SuplementMeasureTypes(
+	SuplementMeasureTypeId int primary key auto_increment,
+    SuplementMeasureTypeName varchar(100) not null unique,
+    SuplementMeasureTypeShortName varchar(20) not null unique
+);
 
 create table Suplements(
 	SuplementId int primary key auto_increment,
     SuplementName varchar(150) not null unique,
-    SuplementManufacturer varchar(150) not null,
-    SuplementManufactureCountry varchar(150) not null,
+    SuplementManufacturer int not null,
     SuplementMeasureType int not null,
-    foreign key (SuplementMeasureType) references SuplementMeasureTypes(SuplementMeasureTypeId)
+    foreign key (SuplementMeasureType) references SuplementMeasureTypes(SuplementMeasureTypeId),
+	foreign key (SuplementManufacturer) references SuplementManufacturers(SuplementManufacturerId)
 );
 
 create table SuplementRewievs(
@@ -72,12 +87,6 @@ create table SuplementRewievs(
     SuplementReviewText varchar(2000) null,
     foreign key (UserId) references Users(UserId),
     foreign key (Suplement) references Suplements(SuplementId)
-);
-
-create table SuplementMeasureTypes(
-	SuplementMeasureTypeId int primary key auto_increment,
-    SuplementMeasureTypeName varchar(100) not null unique,
-    SuplementMeasureTypeShortName varchar(20) not null unique
 );
 
 create table TimesOfConsume(
